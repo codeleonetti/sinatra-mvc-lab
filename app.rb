c776c2935c9e5c48a52c3bee1243d1cbc7c546f3
john <john.d.leonetti@gmail.com>
2021-04-09 11:02:26 -0700

    commit tree 2a80e5967cdd53a58fe5e1b894ff34ab93e86b5f

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        display = PigLatinizer.new
+        @piglatin = display.piglatinize(params[:user_text])
+        erb :display_piglatinizer
+    end
+
 end
